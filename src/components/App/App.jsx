311d2ff9687fc50397b50d7d9aa9740a9bb78d68
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-05-10 11:35:46 +0300

    add + filters

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,51 +2,51 @@ import "./App.css";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const initialContacts = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedData = localStorage.getItem("contacts");
-    return savedData != null ? JSON.parse(savedData) : initialContacts;
-  });
-  const [filter, setFilter] = useState("");
+  // const [contacts, setContacts] = useState(() => {
+  //   const savedData = localStorage.getItem("contacts");
+  //   return savedData != null ? JSON.parse(savedData) : initialContacts;
+  // });
+  // const [filter, setFilter] = useState("");
 
   //Save on Local Storage
 
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem("contacts", JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const filteredContacts =
-    contacts?.filter(
-      (contact) =>
-        contact.name &&
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-    ) || [];
+  // const filteredContacts =
+  //   contacts?.filter(
+  //     (contact) =>
+  //       contact.name &&
+  //       contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   ) || [];
   //Adding new contacts
 
-  const addContact = (newContact) => {
-    setContacts([...contacts, newContact]);
-  };
+  // const addContact = (newContact) => {
+  //   setContacts([...contacts, newContact]);
+  // };
 
   //Delete contact
-  const handleDelete = (id) => {
-    setContacts(contacts.filter((contact) => contact.id !== id));
-  };
+  // const handleDelete = (id) => {
+  //   setContacts(contacts.filter((contact) => contact.id !== id));
+  // };
 
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={filter} onChange={setFilter} />
-      <ContactList contacts={filteredContacts} onDelete={handleDelete} />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,21 @@
+import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "../ContactList/ContactList.module.css";
 
-export default function ContactList({ contacts, onDelete }) {
+export default function ContactList() {
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+  const filteredContacts =
+    contacts?.filter(
+      (contact) =>
+        contact.name &&
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+    ) || [];
+
   return (
     <ul className={css.container}>
-      {contacts.map((contact) => (
-        <Contact key={contact.id} {...contact} onDelete={onDelete} />
+      {filteredContacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
   );
